5d7ad17eadbaaf4f72a380f9d9f8faf3aa7ac1bb
primaeval <primaeval@users.noreply.github.com>
2017-09-04 14:21:42 +0200

    md5

--- go.sh
+++ go.sh
@@ -9,21 +9,22 @@ mkdir $branch
 echo "<?xml version="1.0" encoding="UTF-8" standalone="yes"?>" > $branch/addons.xml
 echo "<addons>" >> $branch/addons.xml
 for addon in $addons; do
-echo $addon
-git clone https://github.com/primaeval/$addon.git
-cd $addon/
-git fetch
-tag=$(git describe --abbrev=0 --tags)
-echo
-echo $tag
-echo
-git checkout $tag
-cd -
-mkdir $branch/$addon/
-for file in "addon.xml changelog.txt icon.png fanart.jpg"; do
-cp $addon/$file $branch/$addon/
-done
-/c/Program\ Files/7-Zip/7z.exe a -xr@exclude $branch/$addon/$addon-$tag.zip $addon
-tail -n+2 $addon/addon.xml >> $branch/addons.xml 
+	echo $addon
+	git clone https://github.com/primaeval/$addon.git
+	cd $addon/
+	git fetch
+	tag=$(git describe --abbrev=0 --tags)
+	echo
+	echo $tag
+	echo
+	git checkout $tag
+	cd -
+	mkdir $branch/$addon/
+	for file in "addon.xml changelog.txt icon.png fanart.jpg"; do
+		cp $addon/$file $branch/$addon/
+	done
+	/c/Program\ Files/7-Zip/7z.exe a -xr@exclude $branch/$addon/$addon-$tag.zip $addon
+	tail -n+2 $addon/addon.xml >> $branch/addons.xml 
 done
 echo "</addons>" >> $branch/addons.xml
+(cd $branch && md5sum addons.xml > addons.xml.md5)
