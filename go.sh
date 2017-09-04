e359c493deabadddfa0376d8987fdf64c71888cd
primaeval <primaeval@users.noreply.github.com>
2017-09-04 14:14:59 +0200

    addons.xml

--- go.sh
+++ go.sh
@@ -4,7 +4,10 @@ addons="
 plugin.video.bbc 
 plugin.audio.bbc
 "
-
+branch=jarvis
+mkdir $branch
+echo "<?xml version="1.0" encoding="UTF-8" standalone="yes"?>" > $branch/addons.xml
+echo "<addons>" >> $branch/addons.xml
 for addon in $addons; do
 echo $addon
 git clone https://github.com/primaeval/$addon.git
@@ -16,5 +19,11 @@ echo $tag
 echo
 git checkout $tag
 cd -
-/c/Program\ Files/7-Zip/7z.exe a -xr@exclude $addon-$tag.zip $addon
+mkdir $branch/$addon/
+for file in "addon.xml changelog.txt icon.png fanart.jpg"; do
+cp $addon/$file $branch/$addon/
 done
+/c/Program\ Files/7-Zip/7z.exe a -xr@exclude $branch/$addon/$addon-$tag.zip $addon
+tail -n+2 $addon/addon.xml >> $branch/addons.xml 
+done
+echo "</addons>" >> $branch/addons.xml
